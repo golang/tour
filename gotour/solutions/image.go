35646bbb715f15c704d8f2410292b6c594c66583
Francesc Campoy <campoy@golang.org>
2012-12-01 11:00:40 -0800

    go-tour/gotour/solution: Adding alternative solution to the image exercise.
    R=golang-dev, adg
    CC=golang-dev
    https://golang.org/cl/6782136

--- gotour/solutions/image.go
+++ gotour/solutions/image.go
@@ -10,38 +10,24 @@ import (
 	"tour/pic"
 )
 
-type Image [][]uint8
+type Image struct {
+	Height, Width int
+}
 
 func (m Image) ColorModel() color.Model {
 	return color.RGBAModel
 }
 
 func (m Image) Bounds() image.Rectangle {
-	dx, dy := len(m), 0
-	if dx > 0 {
-		dy = len(m[0])
-	}
-
-	return image.Rect(0, 0, dx, dy)
+	return image.Rect(0, 0, m.Height, m.Width)
 }
 
 func (m Image) At(x, y int) color.Color {
-	v := m[x][y]
-	return color.RGBA{v, v, 255, 255}
-}
-
-func NewImage(x, y int, f func(int, int) uint8) Image {
-	m := make([][]uint8, x)
-	for i := range m {
-		m[i] = make([]uint8, y)
-		for j := range m[i] {
-			m[i][j] = f(i, j)
-		}
-	}
-	return m
+	c := uint8(x ^ y)
+	return color.RGBA{c, c, 255, 255}
 }
 
 func main() {
-	m := NewImage(255, 255, func(a, b int) uint8 { return uint8(a * b) })
+	m := Image{256, 256}
 	pic.ShowImage(m)
 }
