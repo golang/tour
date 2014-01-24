d9cc0cfd6e426dd8909ce9898d8cf3dd6f2c8aaa
Francesc Campoy <campoy@golang.org>
2014-01-24 13:21:49 -0800

    go-tour: use variables in type conversion example instead of constants
    R=adg, r
    CC=golang-codereviews, golang-dev
    https://golang.org/cl/54900044

--- content/prog/tour/type-conversions.go
+++ content/prog/tour/type-conversions.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(3*3 + 4*4))
+	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z int = int(f)
 	fmt.Println(x, y, z)
 }
