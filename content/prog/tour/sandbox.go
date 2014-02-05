65975571d00d1acfc0bfa297296440d5710b7f8e
Andrew Gerrand <adg@golang.org>
2014-02-05 11:04:40 +0100

    go-tour: update playground slide
    Fixes Go issue 7253.
    
    LGTM=campoy
    R=campoy
    CC=golang-dev
    https://golang.org/cl/59990044

--- content/prog/tour/sandbox.go
+++ content/prog/tour/sandbox.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"os"
 	"time"
 )
 
@@ -11,10 +9,4 @@ func main() {
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now())
-
-	fmt.Println("And if you try to open a file:")
-	fmt.Println(os.Open("filename"))
-
-	fmt.Println("Or access the network:")
-	fmt.Println(net.Dial("tcp", "google.com"))
 }
