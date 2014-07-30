f82b5da64d2d1a0de764ca341592c8ecf83079f1
Andrew Gerrand <adg@golang.org>
2014-07-30 10:48:37 +1000

    go-tour: add Type Inference and Zero Values slides
    Also check off a couple of finished things from the TODO list.
    
    LGTM=campoy
    R=campoy, golang-codereviews
    CC=golang-codereviews
    https://golang.org/cl/116090043

---
+++ content/basics/type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	fmt.Printf("v is of type %T\n", v)
+}
---
+++ content/basics/zero.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
