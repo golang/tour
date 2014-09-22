aaaa9659f10a1c02326b4cfc6820c3750d89c7a5
Francesc Campoy <campoy@golang.org>
2014-09-22 10:27:53 -0700

    go-tour: fix typo in comment
    LGTM=adg
    R=adg, kevlar
    CC=golang-codereviews
    https://golang.org/cl/143310043

--- content/methods/exercise-reader.go
+++ content/methods/exercise-reader.go
@@ -4,7 +4,7 @@ import "code.google.com/p/go-tour/reader"
 
 type MyReader struct{}
 
-// TODO: Add a Read(byte) (int, error) method to MyReader.
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
 
 func main() {
 	reader.Validate(MyReader{})
