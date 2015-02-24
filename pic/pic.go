27c87204043c53a016e3b3282daebaf756e484aa
Andrew Gerrand <adg@golang.org>
2015-02-24 13:52:27 +1100

    all: add import comments
    Change-Id: I30a2b0dec44e3a41e06e418c3d7162da36b60510
    Reviewed-on: https://go-review.googlesource.com/5710
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- pic/pic.go
+++ pic/pic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package pic
+package pic // import "golang.org/x/tour/pic"
 
 import (
 	"bytes"
--- reader/validate.go
+++ reader/validate.go
@@ -1,4 +1,8 @@
-package reader
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package reader // import "golang.org/x/tour/reader"
 
 import (
 	"fmt"
