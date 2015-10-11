500288ff415f380583e271decde1cde70e3348ef
Katrina Owen <katrina.owen@gmail.com>
2015-10-11 11:39:41 -0600

    content: elaborate on function values
    The description was a little bit sparse.
    
    The next slide talks about closures, but since the idea of
    function values wasn't clear to all readers, some people got
    confused when the next slide defined a function that had
    a function as a return value.
    
    Fixes golang/go#4284
    
    Change-Id: Id71f71cafb7c411dbad8306980f7eb1d75aa59e5
    Reviewed-on: https://go-review.googlesource.com/15752
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- content/moretypes/function-values.go
+++ content/moretypes/function-values.go
@@ -7,10 +7,16 @@ import (
 	"math"
 )
 
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
+	fmt.Println(hypot(5, 12))
 
-	fmt.Println(hypot(3, 4))
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
 }
