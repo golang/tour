b9bdbaad7b966081ff67733f07d12f5777c8048e
Katrina Owen <katrina.owen@gmail.com>
2015-10-19 11:23:28 -0600

    content: add an extra slide for slices of slice
    The `Pic` exercise throws people a bit, since they've not seen
    the syntax for slices of slice.
    
    This shows syntax that is similar enough to provide a hint, without
    giving away the answer. They still have to figure out the assignment in
    the loop on their own.
    
    Fixes golang/go#12284
    
    Change-Id: I9f12e97c4ca0ac37d08898f8a898c78b2f983ef4
    Reviewed-on: https://go-review.googlesource.com/16028
    Reviewed-by: Andrew Gerrand <adg@golang.org>

---
+++ content/moretypes/slices-of-slice.go
@@ -0,0 +1,32 @@
+// +build OMIT
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Create a tic-tac-toe board.
+	game := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	game[0][0] = "X"
+	game[2][2] = "O"
+	game[2][0] = "X"
+	game[1][0] = "O"
+	game[0][2] = "X"
+
+	printBoard(game)
+}
+
+func printBoard(s [][]string) {
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%s\n", strings.Join(s[i], " "))
+	}
+}
