c899dcdb12f3a5b636661162ef58589952741839
Katrina Owen <katrina.owen@gmail.com>
2016-02-09 18:12:25 +1100

    content: tweak making-slices example for readability
    Adding a bit of whitespace makes the code example more scannable.
    
    Change-Id: Ib8cc8ff2847c3e89d1cd1fcf1e6a0a0aaa562e81
    Reviewed-on: https://go-review.googlesource.com/19401
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- content/moretypes/making-slices.go
+++ content/moretypes/making-slices.go
@@ -7,10 +7,13 @@ import "fmt"
 func main() {
 	a := make([]int, 5)
 	printSlice("a", a)
+
 	b := make([]int, 0, 5)
 	printSlice("b", b)
+
 	c := b[:2]
 	printSlice("c", c)
+
 	d := c[2:5]
 	printSlice("d", d)
 }
