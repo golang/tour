85fbf322bb6187e1163070c4f77b82f810c5e9e5
brian d foy <brian.d.foy@gmail.com>
2018-09-26 18:45:49 -0400

    Seed the random number generator. GitHub #408

--- content/basics/packages.go
+++ content/basics/packages.go
@@ -5,8 +5,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano()); // ignore this detail for now
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
+
