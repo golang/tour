c150a1ec9130d53f9764524a16d0a0c8c722e715
KyleFin <kfin@google.com>
2018-11-16 12:15:40 -0800

    Rename function for clarity
    Rename function so it isn't the same as the local variable "sum".

--- content/concurrency/channels.go
+++ content/concurrency/channels.go
@@ -4,7 +4,7 @@ package main
 
 import "fmt"
 
-func sum(s []int, c chan int) {
+func sumInts(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
@@ -16,8 +16,8 @@ func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
 	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
+	go sumInts(s[:len(s)/2], c)
+	go sumInts(s[len(s)/2:], c)
 	x, y := <-c, <-c // receive from c
 
 	fmt.Println(x, y, x+y)
