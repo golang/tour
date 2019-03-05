45a20b0d69ef2b06d2edfd97210a0df9ffd7f829
Dmitri Shuralyov <dmitshur@golang.org>
2019-03-05 18:22:19 -0500

    tour: deploy with App Engine Standard on Go 1.11
    This change upgrades the deployment of tour to use the newer
    Go 1.11 runtime. As part of that, the appengine build tag is
    removed (it's no longer set by App Engine), and the GAE_ENV
    environment variable is used to detect when tour is being run
    in App Engine mode.
    
    Set an environment variable in app.yaml to configure the
    x/tools/godoc/golangorgenv package appropriately.
    
    Factor out the static file handlers in local.go, but keep
    static file handlers in app.yaml for improved latency across
    global regions.
    
    Updates golang/go#30486
    
    Change-Id: Ia5bc88aab34fd07bf6ff0785da831180f509156f
    Reviewed-on: https://go-review.googlesource.com/c/tour/+/165537
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- appengine.go
+++ appengine.go
@@ -2,56 +2,43 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build appengine
-
 package main
 
 import (
 	"bufio"
 	"bytes"
 	"io"
+	"log"
 	"net/http"
-	"strings"
-
-	"appengine"
+	"os"
 
 	_ "golang.org/x/tools/playground"
 )
 
-const runUrl = "https://golang.org/compile"
-
-func init() {
-	http.Handle("/lesson/", hstsHandler(lessonHandler))
-	http.Handle("/", hstsHandler(rootHandler))
+func gaeMain() {
+	prepContent = gaePrepContent
+	socketAddr = gaeSocketAddr
 
 	if err := initTour(".", "HTTPTransport"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	http.Handle("/", hstsHandler(rootHandler))
+	http.Handle("/lesson/", hstsHandler(lessonHandler))
+
+	registerStatic(".")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	if err := renderUI(w); err != nil {
-		c.Criticalf("UI render: %v", err)
-	}
-}
-
-func lessonHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	lesson := strings.TrimPrefix(r.URL.Path, "/lesson/")
-	if err := writeLesson(lesson, w); err != nil {
-		if err == lessonNotFound {
-			http.NotFound(w, r)
-		} else {
-			c.Criticalf("tour render: %v", err)
-		}
-	}
-}
-
-// prepContent returns a Reader that produces the content from the given
+// gaePrepContent returns a Reader that produces the content from the given
 // Reader, but strips the prefix "#appengine: " from each line. It also drops
 // any non-blank like that follows a series of 1 or more lines with the prefix.
-func prepContent(in io.Reader) io.Reader {
+func gaePrepContent(in io.Reader) io.Reader {
 	var prefix = []byte("#appengine: ")
 	out, w := io.Pipe()
 	go func() {
@@ -84,9 +71,9 @@ func prepContent(in io.Reader) io.Reader {
 	return out
 }
 
-// socketAddr returns the WebSocket handler address.
+// gaeSocketAddr returns the WebSocket handler address.
 // The App Engine version does not provide a WebSocket handler.
-func socketAddr() string { return "" }
+func gaeSocketAddr() string { return "" }
 
 // hstsHandler wraps an http.HandlerFunc such that it sets the HSTS header.
 func hstsHandler(fn http.HandlerFunc) http.Handler {
