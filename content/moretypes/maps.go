c8ed48d8c028d6e1f3ed481472ca3f6da90561d4
Gian Lorenzo Meocci <glmeocci@gmail.com>
2019-06-19 17:02:42 +0200

    avoiding global variable

--- content/moretypes/maps.go
+++ content/moretypes/maps.go
@@ -8,10 +8,8 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
-
 func main() {
-	m = make(map[string]Vertex)
+	m := make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
