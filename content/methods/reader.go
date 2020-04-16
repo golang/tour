8c29aeacb9c89c83dbea7772e683b09ead2b402d
georgettica <11993626+georgettica@users.noreply.github.com>
2020-04-16 15:11:05 +0300

    add space after comma
    another conversions fix

--- content/methods/reader.go
+++ content/methods/reader.go
@@ -17,7 +17,7 @@ func main() {
 		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
-		if errors.Is(err,io.EOF) {
+		if errors.Is(err, io.EOF) {
 			break
 		}
 	}
