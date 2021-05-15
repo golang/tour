d0bd78686acba82b9b9674aedc94db3c1515385f
Justin Lee <JustinDellAdam@live.com>
2021-05-15 11:35:28 +0800

    Remane OS X to macOS

--- content/flowcontrol/switch.go
+++ content/flowcontrol/switch.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Print("Go runs on ")
 	switch os := runtime.GOOS; os {
 	case "darwin":
-		fmt.Println("OS X.")
+		fmt.Println("macOS.")
 	case "linux":
 		fmt.Println("Linux.")
 	default:
